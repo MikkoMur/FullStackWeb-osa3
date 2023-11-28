c325c84bd3621988e47bfcae047c510a189c4541
Mikko Murhu <mikko.murhu@aalto.fi>
2023-11-28 18:20:50 +0200

    Assignments 3.9-3.11

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FullStackWeb-osa3
+
+Link to the app on render: [https://fullstackweb-osa3-mikmur.onrender.com](https://fullstackweb-osa3-mikmur.onrender.com)
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static('build'))
 app.use(cors())
 
 const custom = morgan((tokens, req, res) => {
