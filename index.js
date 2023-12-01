4ae400ea29e390878b1430250ee4f962debc0dad
Mikko Murhu <mikko.murhu@aalto.fi>
2023-12-01 17:07:46 +0200

    Assignments 3.19-3.21

--- index.js
+++ index.js
@@ -91,19 +91,9 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }
-  if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-  }
   const person = new Person({
     name: body.name,
     number: body.number
@@ -112,6 +102,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -119,6 +110,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
